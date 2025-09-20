404d365becf55ae72228dea3c07f9665a3914640
ankurgupta338 <ankurgupta338@gmail.com>
2025-09-20 12:19:51 +0530

    changed pushed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AI-Doctor",
+    version="0.1.0",
+    author="Ankur Gupta",
+    author_email="ankurgupta338@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
